aa51200641e2fb5ad9d32062e2dca5fafc427666
Elena Guidi <37119745+ElePan@users.noreply.github.com>
2022-04-19 10:54:47 +0200

    Add introduction

---
+++ src/main/kotlin/day1/1-introduction/2 HelloWorld2.kt
@@ -0,0 +1,9 @@
+package day1.`1-introduction`
+
+fun main(args: Array<String>) {
+    println("Hello")
+    //println add a new line at the end of the text
+    println(args)
+
+    print("Hello")
+}
---
+++ src/main/kotlin/day1/1-introduction/3 Imports.kt
@@ -0,0 +1,11 @@
+package day1.`1-introduction`
+
+import kotlin.math.PI
+//static import of methods
+
+fun main() {
+    println("Hello")
+
+    print(PI); print(" it's PI!")
+    // ; is used t put more commands in the same line
+}
